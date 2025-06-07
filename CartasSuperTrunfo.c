cca9579ce9b4e6a291bf234cdec376c22a25472a
gitguerreiro <wesleybonner001@gmail.com>
2025-06-07 02:08:03 +0000

    terminei o printf de entrada  de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,10 +9,22 @@
 int main() {
    printf ("Cartas super trunfo!\n"); // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    int codigo_da_cidade;
+    int populacao;
+    int area;
+    int numero_de_pontos_turisticos;
+    double PIB;
+    char nome[20];
+
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Digite o codigo_da_cidade: \n");
+    scanf("%d", &codigo_da_cidade);
+
+    printf("digite a populacao: \n");
+    scanf ("%d", &populacao);
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
